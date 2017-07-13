a250c2d4f4486a1e91ca271f719089f6059c0806
Helge Hess <devteam@zeezide.de>
2017-07-13 14:27:43 +0200

    Fix name

--- Package.swift
+++ Package.swift
@@ -1,7 +1,7 @@
 import PackageDescription
 
 let package = Package(
-  name: "modswifts",
+  name: "swiftmons",
   dependencies: [
     .Package(url: "https://github.com/NozeIO/Noze.io.git",
              majorVersion: 0, minor: 5)
